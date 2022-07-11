934c594c75b06e2c026ff61adbd1935a3cf0fa82
victorekeleme <ekelemevictor99@gmail.com>
2022-07-11 00:05:50 -0400

    Added something locally and remote

--- app.py
+++ app.py
@@ -3,5 +3,9 @@ def home(name):
     greeting = "Hello" + name
     return greeting
 
+<<<<<<< HEAD
 # Calling function remote
+=======
+# Calling function to remote and locally
+>>>>>>> 06a876a (Added something locally)
 home()
